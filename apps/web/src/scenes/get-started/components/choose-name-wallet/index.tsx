20d62d1f3b7f4eabdedb0d489dfe7d80ef5b0103
Henry Nguyen <henry@pactus.org>
2025-03-24 22:04:10 +0700

    fix: correct UI label typo - Remove redundant console.log statements - Fix spelling in error message

--- apps/web/src/scenes/get-started/components/choose-name-wallet/index.tsx
+++ apps/web/src/scenes/get-started/components/choose-name-wallet/index.tsx
@@ -1,25 +1,143 @@
-'use client'
-import { walletNameLottie } from '@/assets'
-import React from 'react'
-import './style.css'
-import dynamic from 'next/dynamic'
-import { useRestoreWallet, useWallet } from '@/wallet'
+'use client';
+import { walletNameLottie } from '@/assets';
+import React from 'react';
+import './style.css';
+import dynamic from 'next/dynamic';
+import { useRestoreWallet, useWallet } from '@/wallet';
 const LottiePlayer = dynamic(() => import('react-lottie-player'), { ssr: false });
 const ChooseNameWallet = () => {
     const { setWalletName, walletName } = useWallet();
     const { restoreWallet } = useRestoreWallet();
     const emojis = [
-        "😀", "😃", "😄", "😁", "😆", "😅", "😂", "🤣", "🥲", "🥹", "☺️", "😊", "😇", "🙂", "🙃", "😉", "😌",
-        "😍", "🥰", "😘", "😗", "😙", "😚", "😋", "😛", "😝", "😜", "🤪", "🤨", "🧐", "🤓", "😎", "🥸", "🤩",
-        "🥳", "🙂‍↕️", "😏", "😒", "🙂‍↔️", "😞", "😔", "😟", "😕", "🙁", "☹️", "😣", "😖", "😫", "😩", "🥺",
-        "😢", "😭", "😮‍💨", "😤", "😠", "😡", "🤬", "🤯", "😳", "🥵", "🥶", "😱", "😨", "😰", "😥", "😓", "🫣",
-        "🤗", "🫡", "🤔", "🫢", "🤭", "🤫", "🤥", "😶", "😶‍🌫️", "😐", "😑", "😬", "🫨", "🫠", "🙄", "😯", "😦",
-        "😧", "😮", "😲", "🥱", "😴", "🤤", "😪", "😵", "😵‍💫", "🫥", "🤐", "🥴", "🤢", "🤮", "🤧", "😷", "🤒",
-        "🤕", "🤑", "🤠", "😈", "👿", "👹", "👺", "🤡", "💩", "👻", "💀", "☠️", "👽", "👾", "🤖", "🎃", "😺", "😸",
-        "😹", "😻", "😼", "😽", "🙀", "😿", "😾"
+        '😀',
+        '😃',
+        '😄',
+        '😁',
+        '😆',
+        '😅',
+        '😂',
+        '🤣',
+        '🥲',
+        '🥹',
+        '☺️',
+        '😊',
+        '😇',
+        '🙂',
+        '🙃',
+        '😉',
+        '😌',
+        '😍',
+        '🥰',
+        '😘',
+        '😗',
+        '😙',
+        '😚',
+        '😋',
+        '😛',
+        '😝',
+        '😜',
+        '🤪',
+        '🤨',
+        '🧐',
+        '🤓',
+        '😎',
+        '🥸',
+        '🤩',
+        '🥳',
+        '🙂‍↕️',
+        '😏',
+        '😒',
+        '🙂‍↔️',
+        '😞',
+        '😔',
+        '😟',
+        '😕',
+        '🙁',
+        '☹️',
+        '😣',
+        '😖',
+        '😫',
+        '😩',
+        '🥺',
+        '😢',
+        '😭',
+        '😮‍💨',
+        '😤',
+        '😠',
+        '😡',
+        '🤬',
+        '🤯',
+        '😳',
+        '🥵',
+        '🥶',
+        '😱',
+        '😨',
+        '😰',
+        '😥',
+        '😓',
+        '🫣',
+        '🤗',
+        '🫡',
+        '🤔',
+        '🫢',
+        '🤭',
+        '🤫',
+        '🤥',
+        '😶',
+        '😶‍🌫️',
+        '😐',
+        '😑',
+        '😬',
+        '🫨',
+        '🫠',
+        '🙄',
+        '😯',
+        '😦',
+        '😧',
+        '😮',
+        '😲',
+        '🥱',
+        '😴',
+        '🤤',
+        '😪',
+        '😵',
+        '😵‍💫',
+        '🫥',
+        '🤐',
+        '🥴',
+        '🤢',
+        '🤮',
+        '🤧',
+        '😷',
+        '🤒',
+        '🤕',
+        '🤑',
+        '🤠',
+        '😈',
+        '👿',
+        '👹',
+        '👺',
+        '🤡',
+        '💩',
+        '👻',
+        '💀',
+        '☠️',
+        '👽',
+        '👾',
+        '🤖',
+        '🎃',
+        '😺',
+        '😸',
+        '😹',
+        '😻',
+        '😼',
+        '😽',
+        '🙀',
+        '😿',
+        '😾'
     ];
     return (
-        <div className='container-ChooseNameWallet' >
+        <div className="container-ChooseNameWallet">
             <LottiePlayer
                 animationData={walletNameLottie}
                 loop={false}
@@ -27,21 +145,31 @@ const ChooseNameWallet = () => {
                 style={{ height: '200px' }}
             />
             <h1>Name your Wallet</h1>
-            <p>Name yor wallet to easily identify it using the Pactus wallet. these names are stored locally, and can only be seen by you.</p>
-            <div className='input-ChooseNameWallet'>
+            <p>
+                Name your wallet to easily identify it using the Pactus wallet. these names are
+                stored locally, and can only be seen by you.
+            </p>
+            <div className="input-ChooseNameWallet">
                 <input
-                    type='text'
+                    type="text"
                     placeholder="Wallet name"
-                    onChange={(e) => setWalletName(e.target.value)}
+                    onChange={e => setWalletName(e.target.value)}
                 />
-
             </div>
-            <div className='emoji-ChooseNameWallet' >
-                {emojis.map((emoji, index) => (<button key={`${index}-emoji`} >{emoji}</button>))}
+            <div className="emoji-ChooseNameWallet">
+                {emojis.map((emoji, index) => (
+                    <button key={`${index}-emoji`}>{emoji}</button>
+                ))}
             </div>
-            <button className='cta-ChooseNameWallet' disabled={walletName.length == 0} onClick={() => restoreWallet()}  >Finish</button>
+            <button
+                className="cta-ChooseNameWallet"
+                disabled={walletName.length == 0}
+                onClick={() => restoreWallet()}
+            >
+                Finish
+            </button>
         </div>
-    )
-}
+    );
+};
 
-export default ChooseNameWallet
+export default ChooseNameWallet;
