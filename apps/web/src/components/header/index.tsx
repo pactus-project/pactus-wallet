be995b2e266d14be779154763af2e46df4f1d5dc
Henry <hoanguit@gmail.com>
2025-03-31 00:40:39 +0700

    feat: hide un-implemented features (#84)

--- apps/web/src/components/header/index.tsx
+++ apps/web/src/components/header/index.tsx
@@ -1,15 +1,23 @@
 import React from 'react'
 import './style.css'
 import Image from 'next/image';
-import { exportWalletIcon, logoutIcon } from '@/assets';
+import { logoutIcon } from '@/assets';
+import { useWallet } from '@/wallet';
+
 const Header = ({ title }: { title: string; }) => {
+  const { setWallet } = useWallet();
+
+  const handleLogout = () => {
+    localStorage.removeItem('walletStatus');
+    localStorage.removeItem('pactus_wallet_data');
+    setWallet(null);
+    window.location.href = '/get-started';
+  };
+
   return (
     <header className='headerContainer' >
       <h1>{title}</h1>
-      <button className='exportWallet-header' >
-        <Image src={exportWalletIcon} alt='export-wallet-icon' />Export Wallet
-      </button>
-      <button><Image src={logoutIcon} width={32} height={32} alt='logout-icon' /></button>
+      <button onClick={handleLogout}><Image src={logoutIcon} width={32} height={32} alt='logout-icon' /></button>
     </header>
   )
 }
