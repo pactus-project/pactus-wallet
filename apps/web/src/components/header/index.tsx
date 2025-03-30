0b6d0435864f806ab481b207b04cbee07b3cb77b
Henry Nguyen <hoanguit@gmail.com>
2025-03-30 22:13:20 +0700

    feat: add action logout reset the wallet, clear the storage

--- apps/web/src/components/header/index.tsx
+++ apps/web/src/components/header/index.tsx
@@ -2,11 +2,22 @@ import React from 'react'
 import './style.css'
 import Image from 'next/image';
 import { logoutIcon } from '@/assets';
+import { useWallet } from '@/wallet';
+
 const Header = ({ title }: { title: string; }) => {
+  const { setWallet } = useWallet();
+
+  const handleLogout = () => {
+    localStorage.removeItem('walletStatus');
+    localStorage.removeItem('pactus_wallet_data');
+    setWallet(null);
+    window.location.href = '/get-started';
+  };
+
   return (
     <header className='headerContainer' >
       <h1>{title}</h1>
-      <button><Image src={logoutIcon} width={32} height={32} alt='logout-icon' /></button>
+      <button onClick={handleLogout}><Image src={logoutIcon} width={32} height={32} alt='logout-icon' /></button>
     </header>
   )
 }
