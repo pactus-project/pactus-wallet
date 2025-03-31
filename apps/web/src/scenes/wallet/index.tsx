a3835c66f825afed81234c186fac3004b3957605
Henry Nguyen <hoanguit@gmail.com>
2025-03-31 11:10:48 +0700

    feat: show pactus address

--- apps/web/src/scenes/wallet/index.tsx
+++ apps/web/src/scenes/wallet/index.tsx
@@ -1,48 +1,82 @@
-"use client";
-import React, { Suspense } from 'react'
-import './style.css'
+'use client';
+import React, { Suspense, useState } from 'react';
+import './style.css';
 import Sidebar from '@/components/sidebar';
 import Header from '@/components/header';
 import Image from 'next/image';
-import TransactionsHistory from '@/components/transactions-history';
-import { searchIcon, simpleLogo, transactions } from '@/assets';
-import RefetchBalance from '@/components/refetch';
+import { simpleLogo } from '@/assets';
 import SendPac from '@/components/send';
 import BridgePac from '@/components/bridge';
+import { documentCopyIcon, successIcon } from '@/assets';
+import QRCode from 'react-qr-code';
+
 const Wallet = () => {
+    const [copied, setCopied] = useState(false);
+    const accountAddress = 'pc1rek8n7m9hra4w0zlwsmufndrxftemkever9lsdy';
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText('pc1rek8n7m9hra4w0zlwsmufndrxftemkever9lsdy');
+        setCopied(true);
+        setTimeout(() => setCopied(false), 2000);
+    };
+
     return (
         <Suspense fallback={<div>Loading...</div>}>
-            <div className='container-wallet' >
+            <div className="container-wallet">
                 <Sidebar />
-                <div className='content-wallet'>
-                    <Header title='🤝 Account 1' />
-                    <div className='section1-wallet'>
+                <div className="content-wallet">
+                    <Header title="🤝 Account 1" />
+                    <div className="section1-wallet">
                         <div>
-                            <div className='amountSection-wallet' >
-                                <h1>Balance<RefetchBalance /></h1>
-                                <div><Image src={simpleLogo} alt='simple-logo' /><p>45778</p><span>PAC</span></div>
-                                <div><span style={{ fontSize: '15px' }} >≈ 2343.56 USD</span></div>
-                                <div className='amountCtas-wallet' ><SendPac /><BridgePac /></div>
-                            </div>
-                        </div>
-                    </div>
-                    <div className='transactionsContainer-wallet'>
-                        <div className='headerTransaction-wallet' >
-                            <h3>Activity</h3>
-                            <div className='searchTransactions-wallet' >
-                                <Image src={searchIcon} alt='search-icon' /><input placeholder='Search by tx hash or address' />
-                            </div>
-                            <div className='filterTransactions-wallet'><button>1D</button><button>7D</button><button style={{ color: '#FFF' }} >All</button></div></div>
-                        <hr />
-                        <div className='transactions-wallet' >
-                            <TransactionsHistory transactions={transactions} height={'474px'} />
-                        </div>
+                            <div className="address-row">
+                                <div className="qr-code-container">
+                                    <QRCode
+                                        value={accountAddress}
+                                        size={214}
+                                        level="H"
+                                    />
+                                </div>
+                                <div className="amountSection-wallet">
+                                    <h1>Balance</h1>
+                                    <div className="amount-row">
+                                        <Image src={simpleLogo} alt="simple-logo" />
+                                        <p>45778</p>
+                                        <span>PAC</span>
+                                    </div>
+                                    <div>
+                                        <span style={{ fontSize: '15px' }}>≈ 2343.56 USD</span>
+                                    </div>
 
+                                    <div className="address-wallet">
+                                        <div className="address-label">Account Address:</div>
+                                    </div>
+
+                                    <div className="address-row">
+                                        <div className="address-value">
+                                            {accountAddress}
+                                        </div>
+                                        <button title="copy seed" onClick={() => handleCopy()}>
+                                            <Image
+                                                src={copied ? successIcon : documentCopyIcon}
+                                                alt="copy-icon"
+                                                width={16}
+                                                height={16}
+                                            />
+                                        </button>
+                                    </div>
+
+                                    <div className="amountCtas-wallet">
+                                        <SendPac />
+                                        <BridgePac />
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </Suspense>
-    )
-}
+    );
+};
 
-export default Wallet
+export default Wallet;
