32fd16dcc64f89d87bbb39140680a27c5e152301
mohammad mohammadi <mohammad01293@gmail.com>
2025-04-11 11:15:48 +0330

    refactor: improve responsiveness of 3D motion component and update styles for mobile

--- apps/web/src/scenes/get-started/components/3d-motion/index.tsx
+++ apps/web/src/scenes/get-started/components/3d-motion/index.tsx
@@ -5,33 +5,43 @@ const LazySpline = lazy(() => import("@splinetool/react-spline"));
 
 const ThreeDMotion = () => {
     const [showSpline, setShowSpline] = useState(false);
+
+    const handleResize = () => {
+        // Check if the screen width is larger than 768px (not mobile)
+        setShowSpline(window.innerWidth > 768);
+    };
+
     useEffect(() => {
-        // Don't show on mobile
-        const timer = setTimeout(() => {
-            setShowSpline(true);
-        }, 1000);
+        // Initial check
+        handleResize();
 
-        return () => clearTimeout(timer);
+        // Add resize event listener
+        window.addEventListener('resize', handleResize);
 
+        // Cleanup listener on unmount
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
     }, []);
+    if (!showSpline) {
+        return <></>
+    }
     return (
         <div style={{ width: '400px', height: '400px', overflow: 'hidden', position: 'relative' }} >
-
-
             <Suspense>
-                {showSpline && (
-                    <motion.div
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
-                        transition={{ duration: 0.8, delay: 1 }}
-                        style={{ width: '600px', height: '600px', position: 'absolute' }}
-                    >
-                        <LazySpline
-                            scene="https://prod.spline.design/mZBrYNcnoESGlTUG/scene.splinecode"
-                            className="absolute inset-0 w-full h-full origin-top-left flex items-center justify-center"
-                        />
-                    </motion.div>
-                )}
+
+                <motion.div
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 0.8, delay: 1 }}
+                    style={{ width: '600px', height: '600px', position: 'absolute' }}
+                >
+                    <LazySpline
+                        scene="https://prod.spline.design/mZBrYNcnoESGlTUG/scene.splinecode"
+                        className="absolute inset-0 w-full h-full origin-top-left flex items-center justify-center"
+                    />
+                </motion.div>
+
             </Suspense>
         </div>
     )
